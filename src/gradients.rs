244dd446134b63a6f6de2a034596640ed085766e
digitalgreenery <digitalgreenery@proton.me>
2024-09-22 18:55:15 -0600

    Initial release of 2d color visualizer

---
+++ src/gradients.rs
@@ -0,0 +1,101 @@
+use bevy::{
+    prelude::*, render::{render_asset::RenderAssetUsages, render_resource::{AsBindGroup, Extent3d, ShaderRef, TextureDimension, TextureFormat}}, sprite::{Material2d, MaterialMesh2dBundle, Mesh2dHandle}
+};
+
+use prismatic_color::{Color as P_Color, constants as Color_Names};
+
+
+
+pub fn spawn(
+    windows: Query<&Window>,
+    commands: &mut Commands,
+    materials: &mut ResMut<Assets<ColorMaterial>>,
+    meshes: &mut ResMut<Assets<Mesh>>,
+    images: &mut ResMut<Assets<Image>>,
+    color_sets: Vec<Vec<P_Color>>,
+){
+    let scale = (1./2.,1./8.);
+    let (width, height) = (
+        windows.single().width() * scale.0, 
+        windows.single().height() * scale.1
+    );
+    let top = windows.single().height() / 2. * (1. - scale.0);
+    
+    for i in 0..3{
+        let rectangle_mesh = Mesh2dHandle(meshes.add(Rectangle::new(width, height)));
+        let color_pair = color_sets.get(i).expect("Gradient Missing");
+        let (start, end) = (
+            color_pair.get(0).expect("Missing gradient start"), 
+            color_pair.get(1).expect("Missing gradient end"),
+        );
+        let image_handle = images.add(gradient_texture(start, end, width, height));
+        commands.spawn(MaterialMesh2dBundle {
+            mesh: rectangle_mesh,
+            material: materials.add(ColorMaterial{
+                texture: Some(image_handle),
+                ..Default::default()
+            }),
+            transform: Transform::from_xyz(0.0, top - height * 1.5 * i as f32, 0.0 ),
+        ..default()
+        }).insert(crate::VisualizerComponent{});
+    }
+
+}
+
+pub fn generate_colors() -> Vec<Vec<P_Color>> {
+    let gradient_one = vec![
+        Color_Names::BLACK,
+        Color_Names::RED,
+    ];
+    let gradient_two = vec![
+        Color_Names::BLACK,
+        Color_Names::GREEN,
+    ];
+    let gradient_three = vec![
+        Color_Names::BLACK,
+        Color_Names::BLUE,
+    ];
+
+    let gradients = vec![
+        gradient_one, 
+        gradient_two, 
+        gradient_three,
+    ];
+
+    return gradients
+}
+
+fn gradient_texture(
+    start: &P_Color, 
+    end: &P_Color, 
+    width: f32, 
+    height: f32,
+) -> Image {
+    let (width, height) = (width.floor() as usize, height.floor() as usize);
+    let gradient = P_Color::gradient(&start, &end, width);
+
+    let mut texture_data: Vec<u8> = Vec::with_capacity(height * width * 4);
+
+    for _ in 0..height{
+        for y in 0..width {
+            let color = gradient.get(y).expect("Gradient Color Access Invalid");
+            let color_data = color.to_linear_rgb().to_u8_array();
+            texture_data.extend_from_slice(&color_data);
+        }
+    }
+
+    
+    // Create the image from the packed texture data
+    let image = Image::new(
+        Extent3d {
+            width: width as u32,
+            height: height as u32,
+            depth_or_array_layers: 1,
+        },
+        TextureDimension::D2,
+        texture_data,
+        TextureFormat::Rgba8UnormSrgb, 
+        RenderAssetUsages::RENDER_WORLD,
+    );
+    return image;
+}
---
+++ src/main.rs
@@ -0,0 +1,165 @@
+use bevy::prelude::*;
+use prismatic_color::Color as P_Color;
+
+mod hue_wheel;
+mod color_peaks;
+mod gradients;
+
+#[derive(Component, Clone)]
+pub struct VisualizerComponent;
+
+#[derive(Clone)]
+pub enum VisualizerScene{
+    HueWheel,
+    ColorPeaks,
+    Gradients,
+}
+
+
+
+trait ColorVisualizer{
+    fn spawn(
+        &self,
+        windows: Query<&Window>,
+        commands: &mut Commands,
+        materials: &mut ResMut<Assets<ColorMaterial>>,
+        meshes: &mut ResMut<Assets<Mesh>>,
+        images: &mut ResMut<Assets<Image>>,
+        color_sets: Vec<Vec<P_Color>>,
+    );
+    fn despawn(
+        commands: &mut Commands,
+        query: Query<Entity, With<VisualizerComponent>>
+    );
+    fn generate_colors(
+        &self,
+        //Need to add color augmentation
+    ) -> Vec<Vec<P_Color>>;
+}
+
+impl ColorVisualizer for VisualizerScene {
+    fn spawn(
+        &self,
+        windows: Query<&Window>,
+        commands: &mut Commands,
+        materials: &mut ResMut<Assets<ColorMaterial>>,
+        meshes: &mut ResMut<Assets<Mesh>>,
+        images: &mut ResMut<Assets<Image>>,
+        color_sets: Vec<Vec<P_Color>>,
+    ) {
+        match self {
+            VisualizerScene::HueWheel => hue_wheel::spawn(windows, commands, materials, meshes, color_sets),
+            VisualizerScene::ColorPeaks => color_peaks::spawn(windows, commands, materials, meshes, color_sets),
+            VisualizerScene::Gradients => gradients::spawn(windows, commands, materials, meshes, images, color_sets),
+        }
+    }
+
+    fn despawn(
+
+        commands: &mut Commands,
+        query: Query<Entity, With<VisualizerComponent>>
+    ) {
+        for entity in query.iter() {
+            commands.entity(entity).despawn();
+        }
+    }
+
+    fn generate_colors(
+        &self,
+        //Need to add color augmentation
+    ) -> Vec<Vec<P_Color>> {
+        match self {
+            VisualizerScene::HueWheel => hue_wheel::generate_hues(),
+            VisualizerScene::ColorPeaks => color_peaks::generate_colors(),
+            VisualizerScene::Gradients => gradients::generate_colors(),
+        }
+    }
+}
+
+#[derive(Resource)]
+struct SceneConfig {
+    pos: usize,
+    scenes: Vec<VisualizerScene>,
+}
+
+impl SceneConfig {
+    fn new() -> SceneConfig {
+        SceneConfig { 
+            pos: 0,
+            scenes: vec![
+                VisualizerScene::HueWheel,
+                VisualizerScene::ColorPeaks,
+                VisualizerScene::Gradients,
+            ],
+        }
+    }
+    fn advance(&mut self) {
+        self.pos = 
+            if self.pos + 1 < self.scenes.len() { self.pos + 1} else {0};
+    }
+    fn spawn_scene(
+        &self,
+        windows: Query<&Window>,
+        commands: &mut Commands,
+        meshes: &mut ResMut<Assets<Mesh>>,
+        materials: &mut ResMut<Assets<ColorMaterial>>,
+        images: &mut ResMut<Assets<Image>>,
+    ) {
+        let scene = self.scenes.get(self.pos).expect("Scene out of range");
+        let colors = scene.generate_colors();
+        scene.spawn(windows, commands, materials, meshes, images, colors);
+    }
+}
+
+fn main() {
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins)
+    .add_systems(Startup, setup);
+    app.add_systems(Update, toggle_visualizers);
+    app.insert_resource(SceneConfig::new());
+    app.run();
+}
+
+fn setup(
+    windows: Query<&Window>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut images: ResMut<Assets<Image>>,
+    mut scene_config: ResMut<SceneConfig>,
+) {
+    commands.spawn(Camera2dBundle::default());
+
+    *scene_config = SceneConfig::new();
+    scene_config.spawn_scene(windows, &mut commands, &mut meshes, &mut materials, &mut images);
+                            
+    #[cfg(not(target_arch = "wasm32"))]
+    commands.spawn(
+        TextBundle::from_section("Press space to toggle visualizations", TextStyle::default())
+            .with_style(Style {
+                position_type: PositionType::Absolute,
+                top: Val::Px(12.0),
+                left: Val::Px(12.0),
+                ..default()
+            }),
+    );
+}
+
+fn toggle_visualizers(
+    windows: Query<&Window>,
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut images: ResMut<Assets<Image>>,
+    visualizer_components: Query<Entity, With<VisualizerComponent>>,
+    mut scene_config: ResMut<SceneConfig>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+) {
+
+    if keyboard.just_pressed(KeyCode::Space) {
+        VisualizerScene::despawn(&mut commands, visualizer_components);
+        scene_config.advance();
+        scene_config.spawn_scene(windows, &mut commands, &mut meshes, &mut materials, &mut images);
+    }
+}
+ 
